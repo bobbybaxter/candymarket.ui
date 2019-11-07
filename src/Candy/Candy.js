d750d072b40e0ed8db28c7ad4f211cc7ea392120
Bob <bob.e.baxter@gmail.com>
2019-11-06 21:29:36 -0600

    Datatables UI built and readme written

--- src/Candy/Candy.js
+++ src/Candy/Candy.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import $ from 'jquery';
+import 'datatables.net';
 import getAllCandy from '../helpers/dataRequests/CandyRequests';
+
+import './Candy.scss';
 
 class Candy extends React.Component {
   state = {
@@ -8,15 +12,40 @@ class Candy extends React.Component {
 
   componentDidMount() {
     getAllCandy()
-      .then((res) => this.setState({ allCandy: res }))
-      .catch((err) => console.error(err));
+      .then((res) => this.setState({ allCandy: res }));
   }
 
+  printCandyTable = () => {
+    $(this.refs.candyDiv).DataTable({
+      data: this.state.allCandy,
+      columns: [
+        { data: 'id' },
+        { data: 'name' },
+        { data: 'category' },
+        { data: 'manufacturer' },
+      ],
+    });
+  };
+
   render() {
+    if (this.refs.candyDiv) {
+      this.printCandyTable();
+    }
+
     return (
-      <>
-        <h1>All Candy</h1>
-      </>
+      <div className="container">
+        <h1 className="mb-5">Candy Market</h1>
+        <table id="candyDiv" ref="candyDiv" className="display compact">
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>Id</th>
+              <th>Manufacturer</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   }
 }
